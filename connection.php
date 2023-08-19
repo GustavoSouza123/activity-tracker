18c39cd34fa0166da668c2020e24af75de5abfe0
Gustavo Souza <gustavoelia7@gmail.com>
2023-08-18 23:10:46 -0300

    connection file

---
+++ connection.php
@@ -0,0 +1,20 @@
+<?php
+    define('HOST', 'localhost');
+    define('USERNAME', 'root');
+    define('PASSWORD', '');
+
+    try {
+        $pdo = new PDO('mysql:host='.HOST, USERNAME, PASSWORD);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "CREATE DATABASE IF NOT EXISTS `activities`;
+                USE `activities`;
+                CREATE TABLE IF NOT EXISTS `activities` (
+                    id INT NOT NULL AUTO_INCREMENT,
+                    name VARCHAR(55) NOT NULL,
+                    PRIMARY KEY(id)
+                )";
+        $pdo->exec($sql);
+    } catch(PDOException $e) {
+        echo $sql . "<br>" . $e->getMessage();
+    }
+?>
