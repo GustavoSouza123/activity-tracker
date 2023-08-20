c89eb1d9acc3de6a6398e460c16a6e658b23210b
Gustavo Souza <gustavoelia7@gmail.com>
2023-08-20 17:03:32 -0300

    feature: activity data file

---
+++ activity_data.php
@@ -0,0 +1,18 @@
+<?php
+    require "connection.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css" />
+    <title>Activity data</title>
+</head>
+<body>
+    <h2>Activity data</h2>
+
+    <button><a href="index.php">Back to home page</a></button>
+</body>
+</html>
